590956b19009b5b661d54659d517df989c963b38
Soviatul Qhoziyah <soviatulqhoziyah13@gmail.com>
2025-05-07 16:03:55 +0700

    seeder and factory

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -12,6 +13,42 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('categories')->insert([
+            [
+                'category_name' => 'Action',
+                'description' => 'Film dengan adegan-adegan penuh  aksi dan ketegangan',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'category_name' => 'Comedy',
+                'description' => 'Film yang bertujuan untuk menghibur dan mengundang tawa',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'category_name' => 'Drama',
+                'description' => 'Film yang berfokus pada pengembangan karater dan cerita',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'category_name' => 'Sci-Fi',
+                'description' => 'Film dengan latar be;akang ilmiah dan teknologi futuristik',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'category_name' => 'Romance',
+                'description' => 'Film yang berpusat pada kisah cinta dan hubungan romantis',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            
+        ]);
     }
 }
